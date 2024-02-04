cd4e2fda5c8e6bea8b109a4ef66579c7a0dcff70
anishumanisa <anis.humanisa1403@gmail.com>
2024-02-04 16:17:58 +0700

    chekout test cypress

--- cypress.config.js
+++ cypress.config.js
@@ -2,8 +2,14 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
+    baseUrl: 'https://magento.softwaretestingboard.com/',
+    env: {
+      Valid_email: "loremipsum@gmail.com",
+      Valid_password: "lorem123@",
+    },
     setupNodeEvents(on, config) {
       // implement node event listeners here
+      
     },
   },
 });
