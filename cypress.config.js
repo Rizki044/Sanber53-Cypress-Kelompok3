bd549193368e5d282d6acbb286066c16aeb532f7
anishumanisa <anis.humanisa1403@gmail.com>
2024-02-04 16:21:45 +0700

    change config

--- cypress.config.js
+++ cypress.config.js
@@ -3,13 +3,7 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   e2e: {
     baseUrl: 'https://magento.softwaretestingboard.com/',
-    env: {
-      Valid_email: "loremipsum@gmail.com",
-      Valid_password: "lorem123@",
-    },
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-      
-    },
+    specPattern: "cypress/e2e/web/magento/*.cy.js",
+    supportFile: false
   },
 });
