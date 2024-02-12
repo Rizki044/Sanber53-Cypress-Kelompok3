1181556384d13dbb8b2b7344149753959f7837be
umardanimarcelo <muh.umardani1988@gmail.com>
2024-02-12 22:22:29 +0700

    add Remove product from list

--- cypress/e2e/web/magento/update.cy.js
+++ cypress/e2e/web/magento/update.cy.js
@@ -7,19 +7,13 @@ describe('Update Function',() => {
         cy.visit('/checkout/cart/')
     });
 
-    it('Update Product', () => {
-        cy.get('.action.update').click()
-    })
-
-         ///Delete product
-         it('can delete all completed tasks', () => {
-            cy.contains('Remove item').click()
-            cy.get('.todo-list li')
-              .should('have.length', 1)
-              .should('not.have.text', 'Pay electric bill')
-            // Finally, make sure that the clear button no longer exists.
-            cy.contains('Clear completed').should('not.exist')
-          })
+    /// Remove product
+      it('Remove from wishlist product', () => {
+        cy.get(':nth-child(7) > .item-actions > td > .actions-toolbar > .action-delete');
+          cy.get(':nth-child(7) > .item-actions > td > .actions-toolbar > .action-delete').click();  
+    // Finally, make sure that the clear button no longer exists.
+        
+      })
  
      /// Add discount product 
      
