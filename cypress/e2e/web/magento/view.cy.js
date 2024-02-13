0555a46e2dd439de6632e2c9daf296f0791b48a2
umardanimarcelo <muh.umardani1988@gmail.com>
2024-02-13 18:24:03 +0700

    update

--- cypress/e2e/web/magento/view.cy.js
+++ cypress/e2e/web/magento/view.cy.js
@@ -6,10 +6,11 @@ describe('View Function', () => {
         cy.visit('/checkout/cart')
     });
 
+    //Menampilkan List Product yang ada di Shopping Cart
     it('View Product', () => {
         cy.get('.action.showcart').click()
     })
-
+    //Menampilkan Product di Shopping Cart
     it("display product", () => {
       cy.get(':nth-child(3) > .item-info > .item > .product-item-details > .product-item-name > a').click();
       })
